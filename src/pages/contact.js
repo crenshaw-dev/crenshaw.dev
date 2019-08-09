313c29cc79ccb86b76fd181f911036e405e72cc7
Michael Crenshaw <michael@crenshaw.dev>
2019-08-09 17:59:56 -0400

    fix netlify attribute

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
-    <form name="contact" netlify>
+    <form name="contact" method="POST" data-netlify="true">
         <label for="email">Email</label><br />
         <input id="email" type="email" name="email" /><br />
         <label for="message">Message</label><br />
